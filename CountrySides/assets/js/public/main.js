eb0ae778df772d6c6450db04af2669f88a9d93f9
I3atousai <51400176+I3atousai@users.noreply.github.com>
2025-02-12 14:52:57 +0300

    Add files via upload

---
+++ CountrySides/assets/js/public/main.js
@@ -0,0 +1,9 @@
+const BASE_URL = "http://countrysides"
+
+
+// function work_modal(id_modal) {
+//     const modal = document.getElementById(id_modal)
+//     if (modal) {
+//         modal.classList.toggle('active')
+//     }
+// }
---
+++ CountrySides/components/index/main.php
@@ -0,0 +1,9 @@
+<?php 
+
+use App\Helpers\Navigate;
+require_once $_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php";
+session_start();
+?>
+<main>
+   
+</main>
